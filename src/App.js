de687ac00ea11cc3879eb86450507af19a4390c7
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-09 23:22:48 -0400

    Add banner and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.scss';
 import Row from './components/Row';
 import requests from './components/requests';
+import Banner from './components/Banner';
 
 function App() {
   return (
-    <div>
-      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
+    <div className='app'>
+      {/* {Nav} */}
+      <Banner />
+      {/* {Hero} */}
+
+
+      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Action' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -19,13 +19,18 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   return (
     <div className='row'>
-          <h2>{title}</h2>
-          
-          <div className='row__posters'>
-              {movies.map(movie => (
-                  <img className='row__poster' src={`${baseURL}${movie.poster_path}`} alt={movie.name} key={movie.id} />
-                ))}
-          </div>
+      <h2>{title}</h2>
+
+      <div className='row__posters'>
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+            src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+            key={movie.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
